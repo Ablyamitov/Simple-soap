6714608b1d1f0319e22079d705b4e2b969f1a194
Ablyamitov <ablyamitov2003@mail.ru>
2023-12-06 16:42:13 +0300

    init soap

---
+++ src/main/java/com/mysoap/simplesoap/SimpleSoapApplication.java
@@ -0,0 +1,13 @@
+package com.mysoap.simplesoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysoap/simplesoap/SimpleSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysoap.simplesoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
